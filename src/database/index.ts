c0707d1d56c1cb3a8eb985cf47e8744553bb72e8
lumilitas <lucasmengineer@edu.unifor.br>
2021-11-23 15:48:52 -0300

    Alterando o repositório de categoria

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,6 +4,7 @@ interface IOptions {
   host: string;
 }
 
+console.log("Arquivo database");
 getConnectionOptions().then((options) => {
   const newOptions = options as IOptions;
   newOptions.host = "database_ignite"; // Essa opção deverá ser EXATAMENTE o nome dado ao service do banco de dados
