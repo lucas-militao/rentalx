85aec7a951dbead00de63ededc6798fcca222802
lumilitas <lucasmengineer@edu.unifor.br>
2021-12-21 09:03:45 -0300

    Implementando o caso de uso de especificação para carro

--- README.md
+++ README.md
@@ -34,8 +34,6 @@
 
 **RF**
 - Deve ser possível cadastrar uma especificação para um carro.
-- Deve ser possível listar todas especificações.
-- Deve ser possível listar todos os carros.
 
 **RN**
 - Não deve ser possível cadastrar uma especificação para um carro não cadastrado.
---
+++ src/modules/cars/useCases/createCarSpecification/CreateCarSpecification.spec.ts
@@ -0,0 +1,47 @@
+import { CarsRepositoryInMemory } from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory";
+import { AppError } from "@shared/errors/AppError";
+
+import { CreateCarSpecificationUseCase } from "./CreateCarSpecificationUseCase";
+
+let createCarSpecificationUseCase: CreateCarSpecificationUseCase;
+let carsRepository: CarsRepositoryInMemory;
+
+describe("Create Car Specification", () => {
+  beforeEach(() => {
+    carsRepository = new CarsRepositoryInMemory();
+    createCarSpecificationUseCase = new CreateCarSpecificationUseCase(
+      carsRepository
+    );
+  });
+
+  it("should not be able to add a new specification to an non existent the car", async () => {
+    expect(async () => {
+      const car_id = "1234";
+      const specifications_id = ["54321"];
+
+      await createCarSpecificationUseCase.execute({
+        car_id,
+        specifications_id,
+      });
+    }).rejects.toBeInstanceOf(AppError);
+  });
+
+  it("should be able to add a new specification to the car", async () => {
+    const car = await carsRepository.create({
+      brand: "Brand",
+      category_id: "category",
+      daily_rate: 100,
+      description: "Description Car",
+      fine_amount: 60,
+      license_plate: "ABC-1234",
+      name: "Name Car",
+    });
+
+    const specifications_id = ["54321"];
+
+    await createCarSpecificationUseCase.execute({
+      car_id: car.id,
+      specifications_id,
+    });
+  });
+});
