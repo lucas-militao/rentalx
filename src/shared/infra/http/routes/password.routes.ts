065d5497ab9b73c04a2fdb2dc359309bacd786c8
lumilitas <lucasmengineer@edu.unifor.br>
2022-01-17 09:46:24 -0300

    Implementado o reset password

--- src/shared/infra/http/routes/password.routes.ts
+++ src/shared/infra/http/routes/password.routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 
+import { ResetUserPasswordController } from "@modules/accounts/useCases/resetPasswordUser/ResetUserPasswordController";
 import { SendForgotPasswordMailController } from "@modules/accounts/useCases/sendForgotPassowordMail/SendForgotPasswodMailController";
 
 const passwordRoutes = Router();
 
 const sendForgotPasswordMailController = new SendForgotPasswordMailController();
+const resetUserPasswordController = new ResetUserPasswordController();
 
 passwordRoutes.post("/forgot", sendForgotPasswordMailController.handle);
+passwordRoutes.post("/reset", resetUserPasswordController.handle);
 
 export { passwordRoutes };
